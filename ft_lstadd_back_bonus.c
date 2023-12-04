071fd129283f6f7448e0208d722e12ebe20ce5f5
youssef bahij <115738924+ybahij@users.noreply.github.com>
2023-12-05 00:24:05 +0100

    Add files via upload

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybahij <ybahij@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 12:37:10 by ybahij            #+#    #+#             */
+/*   Updated: 2023/11/23 12:37:54 by ybahij           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	if (lst == NULL)
+		return ;
+	last_node = ft_lstlast(*lst);
+	if (*lst)
+		last_node -> next = new;
+	else
+		*lst = new;
+}
